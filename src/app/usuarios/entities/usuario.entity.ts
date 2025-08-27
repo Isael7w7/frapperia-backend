98ce8500cf25f44bcbaf31a411de6675a373b330
Isael7w7 <isaelreyes427@gmail.com>
2025-08-27 17:52:18 -0600

    Cambios para la entidad de usuarios problema que se puso en otro module

--- src/app/usuarios/entities/usuario.entity.ts
+++ src/app/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,24 @@
-export class Usuario {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'Usuarios' })
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  idUsuario: number;
+
+  @Column({ unique: true })
+  nombreUsuario: string;
+
+  @Column()
+  contraseña: string;
+
+  @Column({ type: 'enum', enum: ['admin', 'vendedor'] }) //Cambia el tipo de rol
+  rol: string;
+
+  @CreateDateColumn()
+  fechaCreacion: Date;
+}
