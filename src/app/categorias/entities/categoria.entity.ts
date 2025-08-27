d32af36c472b0451f8bddedf7aefca5c9171d824
Isael7w7 <isaelreyes427@gmail.com>
2025-08-27 16:05:12 -0600

    inicializacion de la entidad

--- src/app/categorias/entities/categoria.entity.ts
+++ src/app/categorias/entities/categoria.entity.ts
@@ -1,1 +1,24 @@
-export class Categoria {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'Usuarios' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  idUsuario: number;
+
+  @Column({ unique: true })
+  nombreUsuario: string;
+
+  @Column()
+  contraseña: string;
+
+  @Column({ type: 'enum', enum: ['admin', 'vendedor'] })
+  rol: string;
+
+  @CreateDateColumn()
+  fechaCreacion: Date;
+}
