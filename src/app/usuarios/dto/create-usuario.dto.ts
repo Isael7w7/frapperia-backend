be6f3848742c130e00a1839209d60210998da8b5
Isael7w7 <isaelreyes427@gmail.com>
2025-08-28 09:11:45 -0600

    dto del create creado

--- src/app/usuarios/dto/create-usuario.dto.ts
+++ src/app/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUsuarioDto {}
+import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+
+export class CreateUsuarioDto {
+  @IsString()
+  @IsNotEmpty()
+  nombreUsuario: string;
+
+  @IsString()
+  @IsNotEmpty()
+  contrasena: string;
+
+  @IsEnum(['admin', 'vendedor'])
+  @IsNotEmpty()
+  rol: 'admin' | 'vendedor';
+}
